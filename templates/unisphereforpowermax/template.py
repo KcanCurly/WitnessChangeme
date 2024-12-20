d29705ff1688934163d38197075e9ad1b3683ab2
KcanCurly <karadumankcan@gmail.com>
2024-12-20 13:37:36 +0300

    random update 3

--- templates/unisphereforpowermax/template.py
+++ templates/unisphereforpowermax/template.py
@@ -6,7 +6,7 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
     username = "smc"
     password = "smc"
 
-    res = requests.post(url + "univmax/restapi/common/login", auth=(username, password), timeout= 15, verify=False)
+    res = requests.post(url + "/univmax/restapi/common/login", auth=(username, password), timeout= 15, verify=False)
     if not "Unauthorized" in res.text and res.status_code == 200:
         with valid_lock:
             with open("witnesschangeme-valid.txt", "a") as file:
