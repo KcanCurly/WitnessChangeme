741360e4c033170868259cf7c70edd4f30ed496b
KcanCurly <karadumankcan@gmail.com>
2024-12-20 13:51:39 +0300

    random update 12

--- templates/unisphereforpowermax/template.py
+++ templates/unisphereforpowermax/template.py
@@ -10,7 +10,7 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
     res = requests.post(url + "/univmax/restapi/common/login", auth=(username, password), timeout= 15, verify=False, cookies=res.cookies, headers={
         # "Origin": url,
         # "Referer": url + "/univmax/",
-        "RefererFullUrl": url + "/univmax/#/login",
+        # "RefererFullUrl": url + "/univmax/#/login",
         "U4V-REST-APP-NAME" : "univmax"
     })
     print(res.text)
