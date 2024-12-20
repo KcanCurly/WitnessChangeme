dda8430f5da87a216fe926517eb1c521f3f8fef5
KcanCurly <karadumankcan@gmail.com>
2024-12-20 13:16:14 +0300

    lights 2

--- templates/oracle-integrated-lights-out-manager/template.py
+++ templates/oracle-integrated-lights-out-manager/template.py
@@ -11,11 +11,12 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
         cred = cred.split(":")
         username = cred[0]
         password = cred[1]
-        extra= '/iPages/loginProcessor.asp'
+        extra = "/iPages/i_login.asp"
+        extra1 = '/iPages/loginProcessor.asp'
         pattern = r'^(https?://[^/]+)'
         match = re.match(pattern, url)
         base_url = match.group(1)
-        res = requests.get(url, verify=False)
+        res = requests.get(url + extra, verify=False, timeout=15)
         soup = BeautifulSoup(res.text, 'html.parser')
 
         scripts = soup.findAll('script')
@@ -25,9 +26,7 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
                 match = re.search(r'"loginToken", "(.*?)"\);', script.string)
                 login_token = match.group(1)
 
-                res = requests.post(base_url + extra, data={"username" : username, "password": password, "loginToken": login_token}, verify=False, timeout=15)
-                print(res.status_code)
-                print(res.text)
+                res = requests.post(base_url + extra1, data={"username" : username, "password": password, "loginToken": login_token}, verify=False, timeout=15)
 
                 if "/iPages/suntab.asp" in res.text and res.status_code == 200:
                     with valid_lock:
