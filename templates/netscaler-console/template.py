23d2d0bccac5a1ad1be97262038e541be4e70dcd
KcanCurly <karadumankcan@gmail.com>
2025-01-15 17:19:46 +0300

    netscaler-console 2

--- templates/netscaler-console/template.py
+++ templates/netscaler-console/template.py
@@ -12,7 +12,7 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
     match = re.match(pattern, url)
     base_url = match.group(1)
 
-    res = requests.post(base_url + extra, data=f"object=%7B%22login%22%3A%7B%22username%22%3A%22{username}%22%2C%22password%22%3A%22{password}%22%7D%7D")
+    res = requests.post(base_url + extra, verify=False, data=f"object=%7B%22login%22%3A%7B%22username%22%3A%22{username}%22%2C%22password%22%3A%22{password}%22%7D%7D")
     if not "Invalid username or password" in res.text:
         with valid_lock:
             with open("witnesschangeme-valid.txt", "a") as file:
