4a09c96bb66fde47d3e53f030c28c3d42b83731a
KcanCurly <karadumankcan@gmail.com>
2025-06-02 11:34:11 +0300

    u

--- templates/zabbix/template.py
+++ templates/zabbix/template.py
@@ -19,7 +19,7 @@ def verify_login(url, valid_lock, valid_template_lock, verbose = False):
             hostname, _, _ = socket.gethostbyaddr(ip)
     except:pass
 
-    if "incorrect user name or password or account is temporarily blocked" not in res.text.lower() and "Remember me for 30 days" not in res.text.lower():
+    if "Incorrect user name or password or account is temporarily blocked" not in res.text and "Remember me for 30 days" not in res.text:
         with valid_lock:
             with open("witnesschangeme-valid.txt", "a") as file:
                 file.write(f"{url}{f" | {hostname}" if hostname else ""} => ZABBIX => {username}:{password}\n")
